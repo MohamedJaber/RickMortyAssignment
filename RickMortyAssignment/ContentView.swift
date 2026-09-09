43efc8a2e9727c94e733b4c3ba932bea0eec384e
Mohamed Jaber <mohamed.jaber@partner.bmwgroup.com>
2026-09-09 07:53:39 +0100

    Initial Commit

---
+++ RickMortyAssignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RickMortyAssignment
+//
+//  Created by Mohamed Jaber on 09/09/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RickMortyAssignment/RickMortyAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickMortyAssignmentApp.swift
+//  RickMortyAssignment
+//
+//  Created by Mohamed Jaber on 09/09/2026.
+//
+
+import SwiftUI
+
+@main
+struct RickMortyAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
